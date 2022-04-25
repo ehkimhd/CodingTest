d57639857cb89a3aea7f78999d8a0bbee2189048
김은혜 <62863817+ehkimhd@users.noreply.github.com>
2022-04-25 15:27:01 +0900

    Create 거리두기확인하기2.kt

---
+++ Programmers/거리두기확인하기2.kt
@@ -0,0 +1,50 @@
+public class Solution {
+
+    fun solution(places: Array<Array<String>>): IntArray {
+        val answer = IntArray(5)
+        for (i in places.indices) {
+            answer[i] = search(places[i]) // place 한줄씩 연산
+        }
+        return answer
+    }
+
+    // P일때 주변에 P면 실패, O일 때 주변에 P가 2개면 실패
+    fun search(place: Array<String>): Int {
+        val detail = convertCharArray(place)
+
+        for (i in detail.indices) {
+            for (j in detail[i].indices) {
+
+                var sum = if (detail[i][j] == 'P') 1 else 0
+                if (detail[i][j] == 'X') continue
+
+                if (i > 0) {
+                    sum += if (detail[i - 1][j] == 'P') 1 else 0
+                }
+
+                if (i < detail.size - 1) {
+                    sum += if (detail[i + 1][j] == 'P') 1 else 0
+                }
+
+                if (j > 0) {
+                    sum += if (detail[i][j - 1] == 'P') 1 else 0
+                }
+
+                if (j < detail[i].size - 1) {
+                    sum += if (detail[i][j + 1] == 'P') 1 else 0
+                }
+
+                if (sum >= 2) {
+                    return 0
+                }
+            }
+        }
+        return 1
+    }
+
+    private fun convertCharArray(stringList: Array<String>): List<CharArray> {
+        val group = arrayListOf<CharArray>()
+        stringList.forEach { group.add(it.toCharArray()) }
+        return group
+    }
+}
